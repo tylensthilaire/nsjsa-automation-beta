04fe894ea8e36457df52c36257a230e576264db7
gazjoy <post@garethjoyce.co.uk>
2022-01-11 14:52:17 +0000

    [Sprint 9] house in order

--- app/routes.js
+++ app/routes.js
@@ -24,7 +24,7 @@ router.use('/agent/stage/1b/', require('./views/agent/stage/1b/_routes'))
 router.use('/agent/stage/shared/', require('./views/agent/stage/shared/_routes'))
 
 router.use('/sprint3/', require('./views/sprint3/_routes'))
-router.use('/sprint3/additional/', require('./views/sprint3/additional/_routes'))
+
 
 router.use('/sprint4/', require('./views/sprint4/_routes'))
 router.use('/sprint4/1-eligibility/', require('./views/sprint4/1-eligibility/_routes'))
@@ -34,6 +34,8 @@ router.use('/sprint5/', require('./views/sprint5/_routes'))
 router.use('/sprint5/i3/', require('./views/sprint5/i3/_routes'))
 
 router.use('/sprint7/', require('./views/sprint7/_routes'))
+
+router.use('/sprint9/additional/', require('./views/sprint9/additional/_routes'))
 
 // router.use('/agent/stage/1a/3-claim-start/', require('./views/agent/stage/1a/3-claim-start/_routes'))
 // router.use('/agent/stage/1a/4-other-benefits/', require('./views/agent/stage/1a/4-other-benefits/_routes'))
