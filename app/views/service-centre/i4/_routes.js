262f4675e18f113ace43c55a3a717d8965075d61
Tylen St Hilaire <28753109+tylensthilaire@users.noreply.github.com>
2022-08-10 17:34:53 +0100

    fix(i6; s16 & 17): fix broken s16, relabel to s21 & s22

--- app/views/service-centre/i4/_routes.js
+++ app/views/service-centre/i4/_routes.js
@@ -24,7 +24,7 @@ router.get('/start', function (req, res) {
         data['claimStatus'] = 'not-registered';
 
     // all processing scenarios
-    } else if (['s5','s6','s7','s8','s9','s10','s11','s12','s13','s14','s15','s16','s17'].includes(answer)) {
+    } else if (['s5','s6','s7','s8','s9','s10','s11','s12','s13','s14','s15','s21','s22'].includes(answer)) {
 
         data['task'] = 'process';
         data['claimant'] = 'ij';
@@ -119,13 +119,13 @@ router.get('/start', function (req, res) {
         data['claimStatus'] = 'not-entitled';
         data['note'] = 1;
 
-    } else if (answer === 's16') {
+    } else if (answer === 's21') {
         // entitlement note
         data['claimant'] = 'ij';
         data['claimStatus'] = 'awaiting-appointment';
         data['entNote'] = 1;
 
-    } else if (answer === 's17') {
+    } else if (answer === 's22') {
         // full time work note
         data['claimant'] = 'ij';
         data['claimStatus'] = 'not-entitled';
@@ -147,12 +147,12 @@ router.post('/confirmation', function (req, res) {
 router.get('/r-view-claim', function (req, res) {
     let data = req.session.data;
 
-    if (['s17'].includes(answer) && data['updated'] === 1) {
+    if (['s22'].includes(answer) && data['updated'] === 1) {
 
         data['claimStatus'] = 'not-eligible';
         data['FTWNote'] = 0;
 
-    } else if (['s16'].includes(answer) && data['updated'] === 1) {
+    } else if (['s21'].includes(answer) && data['updated'] === 1) {
 
         data['entNote'] = 0;
 
@@ -173,7 +173,7 @@ router.post('/nino-search', function (req, res) {
         data['claimStatus'] = 'not-registered';
     
         // all processing scenarios
-    } else if (['s5','s6','s7','s8','s9','s10','s11','s12','s13','s14','s15','s16','s17'].includes(answer)) {
+    } else if (['s5','s6','s7','s8','s9','s10','s11','s12','s13','s14','s15','s21','s22'].includes(answer)) {
 
         data['task'] = 'process';
         data['claimant'] = 'ij';
@@ -269,13 +269,13 @@ router.post('/nino-search', function (req, res) {
         data['claimStatus'] = 'not-entitled';
         data['note'] = 1;
 
-    } else if (answer === 's16') {
+    } else if (answer === 's21') {
         // entitlement note
         data['claimant'] = 'ij';
         data['claimStatus'] = 'awaiting-appointment';
         data['entNote'] = 1;
 
-    } else if (answer === 's17') {
+    } else if (answer === 's22') {
         // full time work note
         data['claimant'] = 'ij';
         data['claimStatus'] = 'not-entitled';
